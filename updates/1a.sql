74bb1ada6219b80582a7dd990d61fb6acd0ee066
Jonathan Garbee <jonathan@garbee.me>
2013-01-08 07:49:15 -0500

    fixed updates

---
+++ updates/1a.sql
@@ -0,0 +1,3 @@
+alter table shop_products add column garbee_preorder boolean;
+alter table shop_products add column garbee_preorder_details text;
+alter table shop_products add column garbee_preorder_date date;
