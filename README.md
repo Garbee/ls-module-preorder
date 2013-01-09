4a94d7ea886c060a16034fe6b902003934726cfe
Jonathan Garbee <jonathan@garbee.me>
2013-01-08 19:27:11 -0500

    Fixed sql query so the proper column is created. Finished v1.0 module.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ls-module-preorder_extension
+
+This is a module for the [LemonStand ecommerce software](http://lemonstand.com). It adds a pre-order tab to the product interface. Within this tab are three fields:
+1. A checkbox for saying of a product is on pre-order or not.
+2. A release date field. This is displayed as a date input.
+3. An extra details input. This is an HTML input field which is rendered as a TinyMCE (WYSIWYG) instance.
+
+
--- classes/garbeepreorder_module.php
+++ classes/garbeepreorder_module.php
@@ -1,10 +1,7 @@
 <?php
 	class GarbeePreorder_Module extends Core_ModuleBase
 	{
-		/**
-		 * Creates the module information object
-		 * @return Core_ModuleInfo
-		 */
+
 		protected function createModuleInfo()
 		{
 			return new Core_ModuleInfo(
@@ -23,14 +20,14 @@
 		{
 			$product->define_column('garbee_preorder', 'Pre-order')->type(db_bool)->defaultInvisible();
 			$product->define_column('garbee_preorder_details', 'Extra details on the preorder')->type(db_text)->invisible()->validation()->fn('trim');
-            		$product->define_column('garbee_release_date', 'Release date for the product')->type(db_date)->defaultInvisible();
+            $product->define_column('garbee_release_date', 'Release date for the product')->type(db_date)->defaultInvisible();
 		}
 
 		public function extend_product_form($product, $context)
 		{
-			$product->add_form_field('garbee_preorder','left')->comment('Is this product available for preorder?')->tab('Pre-order')->renderAs(frm_checkbox);
-			$product->add_form_field('garbee_release_date', 'left')->comment('Release date for the product')->tab('Pre-order')->renderAs(frm_date);
-			$product->add_form_field('garbee_preorder_details','left')->comment('Information to display about this preorder.')->tab('Pre-order')->renderAs(frm_html);
+			$product->add_form_field('garbee_preorder','left')->comment('Is the product for pre-order?')->tab('Pre-order')->renderAs(frm_checkbox);
+			$product->add_form_field('garbee_release_date', 'left')->comment('Release date for the product','above')->tab('Pre-order')->renderAs(frm_date);
+			$product->add_form_field('garbee_preorder_details','left')->comment('Information to display about this preorder.','above')->tab('Pre-order')->renderAs(frm_html);
 		}
 	}
 ?>
